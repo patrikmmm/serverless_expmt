b538535acb842430353f10ef055981aab039cc9e
Patrik Malve <pmalve11@gmail.com>
2022-07-03 16:11:12 +0100

    init

--- src/serverless/__init__.py
+++ src/serverless/__init__.py
@@ -1,13 +1,19 @@
 '''quick and dirty API example using FastAPI'''
 from fastapi import FastAPI
 from mangum import Mangum
+from random_data_gen import *
 __version__ = '0.1.0'
 
+print(gen_customer_list(5))
+
 app = FastAPI()
+
 
 @app.get("/")
 def get_root():
     '''Dirty Demo'''
-    return {"message:": "FastAPI running in a Lambda function"}
+    return {"message:": "falafel sucks"}
 
 handler = Mangum(app)
+
+
